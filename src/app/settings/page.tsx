bf7a70a96080dfe78f6d2c29cc724bc0ddf8e0a3
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-05-01 10:41:25 +0530

    fix: final tweaks

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 import SettingsPage from "./SettingsPage";
-import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import getSession from "@/lib/getSession";
 
