3033ae89e1a0e22fabe32ea65552c468eff4047e
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-05-01 09:43:26 +0530

    feat: add admin role

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,28 @@
+import { auth } from "@/auth";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import getSession from "@/lib/getSession";
+import { get } from "http";
 
 export const metadata: Metadata = {
   title: "Admin",
 };
 
-export default function Page() {
-  // TODO: Redirect non-admin users
+export default async function Page() {
+  const session = await getSession();
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/api/auth/signin?callbackUrl=/admin");
+  }
+
+  if (user.role !== "admin") {
+    return (
+      <main>
+        <p>You are not authorized to view this pahe</p>
+      </main>
+    );
+  }
 
   return (
     <main className="mx-auto my-10 space-y-3">
